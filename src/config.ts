9a89c5fc4f34ca10e0c9bc266c247b0aecee23f6
Andy <andycunn@gmail.com>
2020-03-01 15:04:34 -0800

    forked from graphql-code-generator/packages and added retry wrapper

---
+++ src/config.ts
@@ -0,0 +1,7 @@
+import { RawClientSideBasePluginConfig } from '@graphql-codegen/visitor-plugin-common';
+
+export interface RawGraphQLRequestPluginConfig
+  extends RawClientSideBasePluginConfig {
+  rawRequest?: boolean;
+  retryPolicy?: boolean;
+}
---
+++ src/visitor.ts
@@ -0,0 +1,126 @@
+import {
+  ClientSideBaseVisitor,
+  ClientSideBasePluginConfig,
+  LoadedFragment,
+  getConfigValue,
+  indentMultiline,
+  DocumentMode,
+} from '@graphql-codegen/visitor-plugin-common';
+import autoBind from 'auto-bind';
+import { GraphQLSchema, Kind } from 'graphql';
+import { OperationDefinitionNode } from 'graphql';
+import { RawGraphQLRequestPluginConfig } from './config';
+
+export interface GraphQLRequestPluginConfig extends ClientSideBasePluginConfig {
+  rawRequest: boolean;
+  retryPolicy: boolean;
+}
+
+export class GraphQLRequestVisitor extends ClientSideBaseVisitor<
+  RawGraphQLRequestPluginConfig,
+  GraphQLRequestPluginConfig
+> {
+  private _operationsToInclude: {
+    node: OperationDefinitionNode;
+    documentVariableName: string;
+    operationType: string;
+    operationResultType: string;
+    operationVariablesTypes: string;
+  }[] = [];
+
+  constructor(
+    schema: GraphQLSchema,
+    fragments: LoadedFragment[],
+    rawConfig: RawGraphQLRequestPluginConfig
+  ) {
+    super(schema, fragments, rawConfig, {
+      rawRequest: getConfigValue(rawConfig.rawRequest, false),
+      retryPolicy: getConfigValue(rawConfig.retryPolicy, false),
+    });
+
+    autoBind(this);
+
+    this._additionalImports.push(
+      `import { GraphQLClient } from 'graphql-request';`
+    );
+
+    if (this.config.documentMode !== DocumentMode.string) {
+      this._additionalImports.push(`import { print } from 'graphql';`);
+    }
+
+    if (this.config.rawRequest) {
+      this._additionalImports.push(
+        `import { GraphQLError } from 'graphql-request/dist/src/types';`
+      );
+    }
+
+    if (this.config.retryPolicy) {
+      // TODO: default config now, configurable policy later
+      this._additionalImports.push(
+        `import { RetryWrapper } from 'typescript-graphql-request-with-retries';`
+      );
+    }
+  }
+
+  protected buildOperation(
+    node: OperationDefinitionNode,
+    documentVariableName: string,
+    operationType: string,
+    operationResultType: string,
+    operationVariablesTypes: string
+  ): string {
+    this._operationsToInclude.push({
+      node,
+      documentVariableName,
+      operationType,
+      operationResultType,
+      operationVariablesTypes,
+    });
+
+    return null;
+  }
+
+  public get sdkContent(): string {
+    const allPossibleActions = this._operationsToInclude
+      .map(o => {
+        const optionalVariables =
+          !o.node.variableDefinitions ||
+          o.node.variableDefinitions.length === 0 ||
+          o.node.variableDefinitions.every(
+            v => v.type.kind !== Kind.NON_NULL_TYPE || v.defaultValue
+          );
+        const doc =
+          this.config.documentMode === DocumentMode.string
+            ? o.documentVariableName
+            : `print(${o.documentVariableName})`;
+        if (this.config.rawRequest) {
+          return `${o.node.name.value}(variables${
+            optionalVariables ? '?' : ''
+          }: ${o.operationVariablesTypes}): Promise<{ data?: ${
+            o.operationResultType
+          } | undefined; extensions?: any; headers: Headers; status: number; errors?: GraphQLError[] | undefined; }> {
+                      return client.rawRequest<${
+                        o.operationResultType
+                      }>(${doc}, variables);
+                  }`;
+        } else {
+          return `
+          ${o.node.name.value}(variables${optionalVariables ? '?' : ''}: ${
+            o.operationVariablesTypes
+          }): Promise<${o.operationResultType}> {
+            return withRetries(() => client.request<${
+              o.operationResultType
+            }>(${doc}, variables));
+          }
+`;
+        }
+      })
+      .map(s => indentMultiline(s, 2));
+
+    return `export function getSdk(client: GraphQLClient, withRetries: RetryWrapper) {
+  return {
+${allPossibleActions.join(',\n')}
+  };
+}`;
+  }
+}
